ab5a6d208ec571802567f16938270442e71ae054
Voronin.AA <voralexan@gmail.com>
2020-12-08 01:53:06 +0300

    version 2

--- script.js
+++ script.js
@@ -1,37 +1,24 @@
 var url = 'http://localhost:3000/meters';
 var url2 = 'http://localhost:3000/insert';
+var url3 = "http://localhost:3000/initial_load";
+var url4 = "http://localhost:3000/change_config";
+var url5 = "http://localhost:3000/list_config";
+var valuesSize = 0;
+var values = [];
 
 function insert() {
-    var send = {
-        "name": "meters",
-        "flat": document.getElementById("flat").value,
-        "resources": [
-            {
-                "name": "water",
-                "water": document.getElementById("water").value
-                ,
-            },
-            {
-                "name": "energy",
-                "day": document.getElementById("day").value,
-                "night": document.getElementById("night").value
-            },
-            {
-                "name": "gas",
-                "gas": document.getElementById("gas").value,
-            }
-        ],
-    };
+    var array = [];
+    for (var i = 0; i < values.length; i++) {
+        array.push(document.getElementById(values[i]).value);
+    }
     var oReq = new XMLHttpRequest();
     oReq.open("PUT", url2);
     oReq.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    oReq.send(JSON.stringify(send));
-    if (!document.getElementById('table_div').hidden)
-        queryHandbook(url)
+    oReq.send(JSON.stringify(array));
+    valuesSize = 0;
 }
 
-function queryHandbook(request) {
-
+function loadTable(request) {
     function reqListener(event) {
         var data = JSON.parse(this.responseText);
         var table = document.getElementById("table_data");
@@ -61,9 +48,108 @@ function queryHandbook(request) {
     oReq.addEventListener("load", reqListener);
     oReq.open("GET", request);
     oReq.send();
-    document.getElementById('table_div').hidden = false;
 }
 
-function hideTable() {
+function showTable() {
+    loadTable(url);
+    if (document.getElementById('table_div').hidden != false)
+        document.getElementById('table_div').hidden = false;
+    else
+        document.getElementById('table_div').hidden = true;
+}
+
+function createSubFieldset(data) {
+    var fieldset = document.createElement("fieldset");
+    var legend = fieldset.appendChild(document.createElement("legend"));
+    legend.appendChild(document.createTextNode(data.name));
+    var keys = Object.keys(data);
+    var counter = 0;
+    for (var k in data) {
+        if (counter > 0) {
+            //console.log(k);
+            fieldset.appendChild(document.createTextNode(keys[counter]));
+            var input = fieldset.appendChild(document.createElement("input"));
+            input.type = "text";
+            input.id = valuesSize.toString();
+            valuesSize++;
+            values.push(input.id);
+            console.log(input.id);
+        }
+        counter += 1;
+    }
+    return fieldset;
+}
+
+function createFieldset(data) {
+    var fieldset = document.createElement("fieldset");
+    fieldset.id = "fieldset";
+    var legend = fieldset.appendChild(document.createElement("legend"));
+    legend.appendChild(document.createTextNode(data.name));
+    var keys = Object.keys(data);
+    fieldset.appendChild(document.createTextNode(keys[1]));
+    var input = fieldset.appendChild(document.createElement("input"));
+    input.type = "text";
+    input.id = valuesSize.toString();
+    valuesSize++;
+    values.push(input.id);
+    console.log(input.id);
+    for (var j in data.resources) {
+        fieldset.appendChild(createSubFieldset(data.resources[j]));
+    }
+    valuesSize = 0;
+    return fieldset;
+}
+
+function initialLoad() {
+    function loader(event) {
+        var data = JSON.parse(this.responseText);
+        var form = document.getElementById("form");
+        var fieldset = createFieldset(data);
+        form.appendChild(fieldset);
+    }
+
+    var oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", loader);
+    oReq.open("GET", url3);
+    oReq.send();
+}
+
+function changeConfig(file_name) {
+    values = [];
+    valuesSize = 0;
+
+    function loader(event) {
+        var data = JSON.parse(this.responseText);
+        var form = document.getElementById("form");
+        var fieldset = createFieldset(data);
+        form.appendChild(fieldset);
+    }
+
+    document.getElementById("fieldset").remove();
+    var oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", loader);
+    oReq.open("POST", url4);
+    oReq.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    oReq.send(JSON.stringify([file_name]));
     document.getElementById('table_div').hidden = true;
+}
+
+function getConfigs() {
+    document.getElementById("list").style.display = 'block';
+
+    function loader(event) {
+        var data = JSON.parse(this.responseText);
+        var list = document.getElementById("list");
+        list.innerHTML = '';
+        item = document.createElement('option');
+        for (var i = 0; i < data.length; i++) {
+            item.innerHTML = data[i];
+            list.appendChild(item.cloneNode(true));
+        }
+    }
+
+    var oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", loader);
+    oReq.open("GET", url5);
+    oReq.send();
 }
--- serverDB.js
+++ serverDB.js
@@ -2,10 +2,34 @@ var express = require("express");
 var app = express();
 var connection = require("./db");
 var bodyParser = require('body-parser');
+var fs = require("fs");
+var globalValues = [];
+var defaultConfig = 'test.json';
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
+
+
+function parseSubConfig(data) {
+    var keys = Object.keys(data);
+    var counter = 0;
+    for (var k in data) {
+        if (counter > 0) {
+            globalValues.push(keys[counter]);
+        }
+        counter += 1;
+    }
+
+}
+
+function parseConfig(data) {
+    var keys = Object.keys(data);
+    globalValues.push(keys[1]);
+    for (var j in data.resources) {
+        parseSubConfig(data.resources[j]);
+    }
+}
 
 app.get("/meters", function (req, res) {
     connection.query("select * from meters", function (err, rows) {
@@ -18,16 +42,67 @@ app.get("/meters", function (req, res) {
 
 });
 
+
 app.put("/insert", function (req, res) {
-    connection.query("insert into meters values (?, ?, ?, ?, ?)",
-        [req.body.flat, req.body.resources[0].water, req.body.resources[1].day,
-            req.body.resources[1].night, req.body.resources[2].gas], function (err, rows) {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            res.end(JSON.stringify(rows));
-        });
+    var str = "insert into meters values (";
+    for (var i = 0; i < req.body.length; i++) {
+        str += req.body[i];
+        if (i != req.body.length - 1) str += ", "
+    }
+    str += ")";
+    connection.query(str, function (err, rows) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        res.end(JSON.stringify(rows));
+    });
+});
+
+app.get("/initial_load", function (req, res) {
+    str = "./configs/" + defaultConfig;
+    var json = require(str);
+    res.end(JSON.stringify(json));
+});
+
+app.post("/change_config", function (req, res) {
+    globalValues = [];
+    valuesSize = 0;
+    path = './configs/';
+    path += req.body;
+    defaultConfig = req.body;
+    var json = require(path);
+    parseConfig(json);
+    str = "create table meters( "
+    connection.query("drop table meters", function (err, rows) {
+        if (err) {
+            console.log(err);
+
+        }
+    });
+    for (var i = 0; i < globalValues.length; i++) {
+        str += globalValues[i];
+        if (i != globalValues.length - 1) {
+            str += " varchar(20), ";
+        } else {
+            str += " varchar(20));";
+        }
+    }
+    console.log(str);
+    connection.query(str, function (err, rows) {
+        if (err) {
+            console.log(err);
+
+        }
+    });
+    res.end(JSON.stringify(json));
+});
+
+app.get("/list_config", function (req, res) {
+    console.log("GET /list_config called");
+    fs.readdir("./configs", function (err, items) {
+        res.end(JSON.stringify(items));
+    });
 });
 
 app.listen(3000);
